6b94086dd0abda700e15a265288c831fe3b59bff
FilipeBeghini <filipe.beghini@users.noreply.github.com>
2024-10-09 23:01:15 +0100

    get database based on the current branch

--- auxiliary.py
+++ auxiliary.py
@@ -1,7 +1,12 @@
 # Header>
 import pyautogui
 import time
+from config import get_database
 
 # get position needed to click with the actual cursor position
 time.sleep(3)
 print(pyautogui.position())
+
+
+database = get_database()
+print (database)
